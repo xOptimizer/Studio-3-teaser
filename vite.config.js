e98256e1ddedbf68c7419cd632ec18648a2db90e
sahil <sahilsinha894@gmail.com>
2025-12-04 18:34:04 +0530

    senetary

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "jsm-x9",
-    project: "javascript-react"
+    project: "javascript-react",
+    telemetry: false
   })],
 
   build: {
